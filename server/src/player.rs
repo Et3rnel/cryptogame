a4083ddbf432fb29a7d8bd0d400bf77021fbc972
g_pasquion <gpasquion.externe@bedrockstreaming.com>
2024-03-29 23:23:33 +0100

    Improve on error comment when UUID parsing fails

--- server/src/player.rs
+++ server/src/player.rs
@@ -1,3 +1,8 @@
+use rand::Rng;
+use std::collections::HashMap;
+
+use crate::game::Canvas;
+
 pub struct Player {
     pub x: f64,
     pub y: f64,
@@ -24,4 +29,38 @@ impl Player {
         self.x += rad.cos() * step_size;
         self.y += rad.sin() * step_size;
     }
+}
+
+pub fn calculate_player_position(
+    existing_players: &HashMap<String, Player>,
+    canvas: &Canvas,
+) -> (f64, f64) {
+    let width = f64::from(canvas.width);
+    let height = f64::from(canvas.height);
+
+    let mut rng = rand::thread_rng();
+    let margin = 0.2; // 20% margin
+    let min_distance = 0.1 * f64::min(width, height); // 10% of the smallest dimension
+
+    let min_x = width * margin;
+    let max_x = width * (1.0 - margin);
+    let min_y = height * margin;
+    let max_y = height * (1.0 - margin);
+
+    'attempt: for _ in 0..100 {
+        let new_x = rng.gen_range(min_x..max_x);
+        let new_y = rng.gen_range(min_y..max_y);
+
+        for player in existing_players.values() {
+            let distance = ((player.x - new_x).powi(2) + (player.y - new_y).powi(2)).sqrt();
+            if distance < min_distance {
+                continue 'attempt;
+            }
+        }
+
+        return (new_x, new_y);
+    }
+
+    error!("Cannot find a matching start position for player after 100 tries.");
+    return (width/2, height/2); // default to the middle of the canvas
 }
